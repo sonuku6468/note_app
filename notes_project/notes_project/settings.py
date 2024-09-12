723204a8d780aeaeff6273021d8065417a9e14e0
sonuku6468 <108977415+sonuku6468@users.noreply.github.com>
2024-09-12 09:34:57 +0530

    "commit"

--- notes_project/notes_project/settings.py
+++ notes_project/notes_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'notes',
+    'drf_yasg', 
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,9 @@ WSGI_APPLICATION = "notes_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
